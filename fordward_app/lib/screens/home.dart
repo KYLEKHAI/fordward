63a4d9b02424edb6a0c908c32905270310ea7465
Kyle Khai Tran <kylekhai04@gmail.com>
2024-03-02 10:04:39 -0500

    Add bottom navigation for home.dart

--- fordward_app/lib/screens/home.dart
+++ fordward_app/lib/screens/home.dart
@@ -8,10 +8,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // KEEP TRACK OF SELECTED INDEX
+  int _selectedIndex = 0;
+
+// ON TAP FUNCTION
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF272849),
+      body: Center(
+        child: Text('Dashboard', style: TextStyle(color: Colors.white)),
+      ),
+
+      // BOTTOM NAVIGATION BAR
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF4D5074),
+
+        // SELECTED ITEM STYLES
+        selectedItemColor: Color(0xFF6ADED7),
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+
+        // BOTTOM NAVIGATION BAR ITEMS
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
