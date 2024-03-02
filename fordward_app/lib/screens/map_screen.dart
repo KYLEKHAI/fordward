539c57870009c93a3381e37ee96f42730fa4dbdd
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-03-02 15:07:09 -0500

    map page update

--- fordward_app/lib/screens/map_screen.dart
+++ fordward_app/lib/screens/map_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fordward_app/screens/home.dart';
-import 'package:fordward_app/screens/profile_screen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:fordward_app/screens/home.dart'; // Assuming you have a HomeScreen widget
+import 'package:fordward_app/screens/profile_screen.dart'; // Assuming you have a ProfilePage widget
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
+  const MapPage({Key? key}) : super(key: key);
 
   @override
   State<MapPage> createState() => _MapState();
@@ -13,7 +14,7 @@ class _MapState extends State<MapPage> {
   // KEEP TRACK OF SELECTED INDEX
   int _selectedIndex = 1;
 
-// ON TAP FUNCTION
+  // ON TAP FUNCTION
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -27,58 +28,105 @@ class _MapState extends State<MapPage> {
           MaterialPageRoute(builder: (context) => HomeScreen()),
         );
         break;
-
-      // MAP SELECTION
       case 1:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => MapPage()),
-        );
+        // Do nothing, we're already on MapPage
         break;
-
-      // PROFILE SELECTION
       case 2:
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => ProfilePage()),
         );
+        break;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF272849),
-      body: Center(
-        child: Text('Map', style: TextStyle(color: Colors.white)),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Set the primary color to blue
       ),
-
-      // BOTTOM NAVIGATION BAR
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF4D5074),
-
-        // SELECTED ITEM STYLES
-        selectedItemColor: Color(0xFF6ADED7),
-        unselectedItemColor: Colors.white,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-
-        // BOTTOM NAVIGATION BAR ITEMS
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: 'Dashboard',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
-          ),
-        ],
+      home: Scaffold(
+        backgroundColor: Color(0xFF272849),
+        body: Stack(
+          children: [
+            MapBackground(),
+            Positioned(
+              bottom: 50.0,
+              right: 40.0,
+              child: SizedBox(
+                width: 50.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true, // Set to true to cover the entire width of the screen
+                      builder: (BuildContext context) {
+                        return Container(
+                          height: 500,
+                          width: 2000, // Set width to cover the entire width of the screen
+                          child: Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white, // Set button color to yellow
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100.0),
+                                ),
+                              ),
+                              child: Text('Close'),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF272849), // Set button color to transparent
+                    shape: CircleBorder(), // Make the button circular
+                  ),
+                  child: Icon(Icons.add, color: Colors.white), // Display "+" icon in yellow
+                ),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF4D5074),
+          selectedItemColor: Color(0xFF6ADED7),
+          unselectedItemColor: Colors.white,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard),
+              label: 'Dashboard',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class MapBackground extends StatelessWidget {
+  static const LatLng _pGooglePlex = LatLng(45.42196276943317, -75.68190641134954);
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 14),
     );
   }
 }
